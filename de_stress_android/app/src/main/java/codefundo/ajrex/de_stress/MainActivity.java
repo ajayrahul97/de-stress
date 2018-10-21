cf800177c7253550fa378ac69c7ef7221523d162
Ajay Rahul <ajayrahul97@gmail.com>
2018-10-21 23:57:46 +0530

    Adds basic activities

--- de_stress_android/app/src/main/java/codefundo/ajrex/de_stress/MainActivity.java
+++ de_stress_android/app/src/main/java/codefundo/ajrex/de_stress/MainActivity.java
@@ -1,13 +1,54 @@
 package codefundo.ajrex.de_stress;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button reg, map, scan;
+
+
+        reg = (Button) findViewById(R.id.btn_reg);
+        map = (Button) findViewById(R.id.btn_map);
+        scan = (Button) findViewById(R.id.btn_scan);
+
+
+        reg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, RegActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, MapActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ScanActivity.class);
+                startActivity(i);
+
+            }
+        });
+
+
+
     }
 }
